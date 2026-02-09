c96e50e1881846032a88052bf2df208598c419e7
Lionel ATTY <yoyonel@hotmail.com>
2026-02-09 17:06:09 +0100

    test: update test integration with ASan
    Signed-off-by: Lionel ATTY <yoyonel@hotmail.com>

--- scripts/test_integration_asan.sh
+++ scripts/test_integration_asan.sh
@@ -57,14 +57,46 @@ xdotool windowactivate "$WID" || echo "Warning: windowactivate failed (expected 
 echo "Starting Integration Test Scenario..."
 # sleep 1
 
+# 0. Activate Functions
+
+echo "=> FPS Counter (F1)"
+for i in {1..6}; do
+    xdotool key --delay 200 F1
+done
+sleep 1
+
+echo "=> Help (F2)"
+xdotool key --delay 200 F2
+sleep 1
+xdotool key --delay 200 F2
+
+echo "=> Activate GPU Profiler (F3)"
+xdotool key --delay 200 F3
+sleep 1
+
+echo "=> Log on GPU Metrics (F4)"
+xdotool key --delay 200 F4
+sleep 1
+
+echo "=> Resetting Camera (R)"
+xdotool key --delay 200 R
+sleep 1
+
+echo "=> Test Full Screen (F)"
+xdotool key --delay 200 F
+sleep 1
+echo "=> Test Windowed (F)"
+xdotool key --delay 200 F
+sleep 1
+
 # 1. Environment Switching
 echo "=> Switching Environments (Page_Up / Page_Down)"
 xdotool key --delay 200 Page_Up
-sleep 2
+sleep 1
 xdotool key --delay 200 Page_Up
-sleep 2
+sleep 1
 xdotool key --delay 200 Page_Down
-sleep 2
+sleep 1
 
 # 2. Styles
 echo "=> Testing Styles (1-6)"
@@ -82,6 +114,8 @@ xdotool key --delay 500 g # Grain
 xdotool key --delay 500 b # Bloom
 xdotool key --delay 500 h # DoF
 xdotool key --delay 500 j # Auto Exposure
+xdotool key --delay 500 m # Motion Blur
+xdotool key --delay 500 x # FXAA
 
 xdotool key --delay 500 w # Wireframe ON
 xdotool key --delay 500 w # Wireframe OFF
@@ -92,19 +126,28 @@ xdotool keydown z; sleep 0.5; xdotool keyup z
 xdotool keydown d; sleep 0.5; xdotool keyup d
 xdotool keydown s; sleep 0.5; xdotool keyup s
 xdotool keydown a; sleep 0.5; xdotool keyup a
+xdotool keydown q; sleep 0.5; xdotool keyup q
+xdotool keydown e; sleep 0.5; xdotool keyup e
 
 # 5. PBR Debug Modes
 echo "=> Cycling PBR Debug Modes (F5)"
-for i in {1..5}; do
+for i in {1..9}; do
     xdotool key --delay 300 F5
 done
 
 # 6. Performance Mode
 echo "=> Toggling Performance Mode (F9)"
 xdotool key --delay 500 F9
-sleep 2
+sleep 1
 xdotool key --delay 500 F9
 # sleep 1
+
+# 7. Style 7 - Banding Modes
+echo "=> Testing Style 7 - Banding Modes"
+for i in {1..4}; do
+    xdotool key --delay 500 7
+done
+
 
 echo "=> Test Complete. Exiting..."
 xdotool key Escape
