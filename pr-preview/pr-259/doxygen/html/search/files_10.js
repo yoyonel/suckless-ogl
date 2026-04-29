dccb0ee422f650853b7400a05243dc2281d29d59
yoyonel <yoyonel@users.noreply.github.com>
2026-04-29 13:55:26 +0000

    Deploy preview for PR 259 🛫

---
+++ pr-preview/pr-259/doxygen/html/search/files_10.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['window_2ec_2615',['window.c',['../window_8c.html',1,'']]],
+  ['window_2eh_2616',['window.h',['../window_8h.html',1,'']]]
+];
---
+++ pr-preview/pr-259/doxygen/html/structPcg32.js
@@ -0,0 +1,5 @@
+var structPcg32 =
+[
+    [ "inc", "structPcg32.html#aa97421a198956761a9390bc4cffc70fa", null ],
+    [ "state", "structPcg32.html#adb4c215369532bb9846b6f60f30f5c75", null ]
+];
